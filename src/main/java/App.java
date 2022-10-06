24b080fa8911e1e4c91e97b877a50ef0e05a5d1c
elenaisakova <lenais1378@gmail.com>
2022-10-06 17:54:52 +0300

    first com

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1.getName());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        System.out.println(bean==bean2);
+        System.out.println(bean1==bean3);
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,8 +1,7 @@
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan()
 public class AppConfig {
  
     @Bean(name="helloworld")
@@ -10,5 +9,12 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat getCat() {
+        Cat cat = new Cat("Murzik", 2);
+        cat.setName("Barsik");
+        return cat;
     }
 }
